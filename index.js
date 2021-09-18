4e58b31d33b95ac871e27fa6afd97c31b38c248a
Esteban Naón <estinaon@gmail.com>
2021-09-18 20:19:30 -0300

    cambios en el responsive y en js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const contenedorNovedades = document.getElementById("novedades-container");
 const flechaDerechaNovedades = document.getElementById("flecha-derecha");
 const flechaIzquierdaNovedades = document.getElementById("flecha-izquierda");
-const novedadesWidth = document.getElementById('novedades-container').offsetWidth;
 
 flechaDerechaNovedades.addEventListener("click", desplazarDerecha);
 flechaIzquierdaNovedades.addEventListener("click", desplazarIzquierda);
 
 
 function desplazarDerecha() {
+    let novedadesWidth = document.getElementById('novedades-container').offsetWidth;
     contenedorNovedades.scrollLeft += novedadesWidth;
 }
 
 function desplazarIzquierda() {
+    let novedadesWidth = document.getElementById('novedades-container').offsetWidth;
     contenedorNovedades.scrollLeft -= novedadesWidth;
 }
