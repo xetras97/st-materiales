633064b4af377d66502d078a3e238afe8225fbbd
Esteban Naón <estinaon@gmail.com>
2021-09-21 17:49:39 -0300

    se agrego badge de precios y se corrigio flechas en xsmall

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 // Variables para slider de novedades //
 const contenedorNovedades = document.getElementById("novedades-container");
-const flechaDerechaNovedades = document.getElementById("flecha-derecha");
-const flechaIzquierdaNovedades = document.getElementById("flecha-izquierda");
-
-// Listeners para slider de novedades //
-flechaDerechaNovedades.addEventListener("click", desplazarDerecha);
-flechaIzquierdaNovedades.addEventListener("click", desplazarIzquierda);
 
 // Funciones para slider de novedades //
 function desplazarDerecha() {
