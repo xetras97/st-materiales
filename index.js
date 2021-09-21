5e6d2c0f30e0ce497ddc6d016c1a40a540b6461e
Esteban Naón <estinaon@gmail.com>
2021-09-21 19:36:49 -0300

    agregada barra de envios y pagos

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+//Inicializar ToolTips
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
 // Variables para slider de novedades //
 const contenedorNovedades = document.getElementById("novedades-container");
 
