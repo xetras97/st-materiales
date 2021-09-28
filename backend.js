4629ff05a12a4ee527735f988ca4b53b054e368d
Esteban Naón <estinaon@gmail.com>
2021-09-28 19:38:31 -0300

    backend + google sheets

--- backend.js
+++ backend.js
@@ -1,42 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const repository = require("./repository");
+const app = express();
+const port = 3000;
 
-const products = [
-    {
-        id: 1,
-        name: "hola",
-        image: "./assets/materiales/192f239f-aadf-45bb-9eb5-afd22059ab31.jpg",
-        price: 50,
-    },
-    {
-        id: 2,
-        name: "hola",
-        image: "./assets/materiales/192f239f-aadf-45bb-9eb5-afd22059ab31.jpg",
-        price: 50,
-    },
-    {
-        id: 3,
-        name: "hola",
-        image: "./assets/materiales/192f239f-aadf-45bb-9eb5-afd22059ab31.jpg",
-        price: 50,
-    },
-    {
-        id: 4,
-        name: "hola",
-        image: "./assets/materiales/192f239f-aadf-45bb-9eb5-afd22059ab31.jpg",
-        price: 50,
-    },
-    {
-        id: 5,
-        name: "hola",
-        image: "./assets/materiales/192f239f-aadf-45bb-9eb5-afd22059ab31.jpg",
-        price: 50,
-    },
-]
-
-app.get('/api/products', (req, res) => {
-  res.send(products)
+app.get('/api/products', async (req, res) => {
+  res.send(await repository.read());
 })
 
 app.use("/", express.static("public"));
--- public/index.js
+++ public/index.js
@@ -7,25 +7,50 @@ const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 window.onload = async() => {
   const productsList = await (await fetch ("/api/products")).json();
   console.log(productsList);
-  displayProducts(productsList);
+  displayNovedades(productsList);
+  displayPromociones(productsList);
 }
 //Funcion para mostrar productos en el html desde base de datos
-function displayProducts (productsList) {
+function displayNovedades (productsList) {
   let productsHTML = ``;
   productsList.forEach(element => {
-    productsHTML += 
-    `<div class="col-6 col-md-4 col-lg-3 mt-2 card-container">
-      <div class="card">
-        <img src="${element.image}" class="img-fluid card-img-top" alt="...">
-        <div class="card-body">
-          <span class="badge text-dark badge-price">$${element.price}</span>
-          <h5 class="card-title">${element.name}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-        </div>
-      </div>
-    </div>`
+    if (element.new === "Novedad") {
+      productsHTML +=
+        `<div class="col-6 col-md-4 col-lg-3 mt-2 card-container">
+          <div class="card">
+            <img src="${element.image}" class="img-fluid card-img-top" alt="${element.name}">
+            <div class="card-body">
+              <span class="badge text-dark badge-price">$${element.price}</span>
+              <h5 class="card-title">${element.name}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${element.description}</h6>
+            </div>
+          </div>
+        </div>`
+    }
   });
   document.getElementById("novedades-container").innerHTML += productsHTML;
+}
+
+function displayPromociones (productsList) {
+  let promocionesHTML = ``;
+  productsList.forEach(element => {
+    if (element.category === "Promociones") {
+      promocionesHTML +=
+        `<div class="col-8 offset-2 col-sm-6 offset-sm-0 col-md-4">
+            <a href="#">
+              <div class="card bg-dark text-white">
+                <img src="${element.image}" class="card-img" alt="${element.name}">
+                <div class="card-img-overlay d-flex flex-column justify-content-end">
+                  <h5 class="card-title">${element.name}</h5>
+                  <p class="card-text">${element.description}</p>
+                </div>
+              </div>
+            </a>
+          </div>
+        `
+    }
+  });
+  document.getElementById("promo-container").innerHTML += promocionesHTML;
 }
 
 // Variables para slider de novedades //
@@ -42,3 +67,4 @@ function desplazarIzquierda(contenedor) {
     let width = contenedor.offsetWidth;
     contenedor.scrollLeft -= width;
 }
+
