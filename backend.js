961c08fcb81ab2b0dfcd8ac79216389e5a89562b
Esteban Naón <estinaon@gmail.com>
2021-10-19 17:47:38 -0300

    agregado archivo procfile y cambio en puero backend

--- backend.js
+++ backend.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const repository = require("./repository");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/api/products', requestProducts(), (req, res) => {
   res.json(res.products);
